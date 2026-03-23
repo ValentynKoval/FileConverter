242e3a36160434ee0ddf8da02b159defcc4cd225
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-23 19:40:32 +0200

    create main method with file processing and folder checker

--- src/main/java/com/file/converter/Main.java
+++ src/main/java/com/file/converter/Main.java
@@ -1,4 +1,16 @@
 package com.file.converter;
 
+import com.file.converter.service.FileProcessor;
+import com.file.converter.service.impl.FileProcessorImpl;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("File Converter v 1.0");
+        FileProcessor fileProcessor = new FileProcessorImpl();
+
+        fileProcessor.folderFileChecker("D:\\JavaProCourse\\checkFolder",
+                "D:\\JavaProCourse\\checkOutputFolder");
+    }
 }
